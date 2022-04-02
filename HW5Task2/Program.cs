04707f328d3c33b7fe92cc0e6c4ea4d7c7c3bc69
Alex <chugun777akk@mail.ru>
2022-04-02 12:32:58 +0700

    Task2 is done

---
+++ HW5Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать числа Фибоначчи
+int n1 = 0;
+int n2 = 1;
+int temp;
+Console.Write("ВВедите число до которого будут считаться числа Фибоначи  ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(n1 + " " + n2 + " ");
+    while (n1 + n2 <= num)
+{
+    Console.Write((n1) + (n2) + " ");
+    temp = n2;
+    n2 = n1 + n2;
+    n1 = temp;
+}
+
+
+
